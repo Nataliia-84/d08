08af1657282e04a69d872439484d8e770382fb01
Nataliia Korotych <natalya.cherlenyuk@gmail.com>
2023-04-04 11:53:05 +0200

    add

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,22 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+// gallery
+
+const listEl=document.querySelector('.gallery');
+console.log(listEl)
+function creatGalery(){
+
+const itemEl= galleryItems.map((item)=>`<li class="gallery__item">
+<a class="gallery__link" href="${original}">
+  <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${originalg}"
+    alt="${description}"
+  />
+</a>
+</li>`)
+}
+
